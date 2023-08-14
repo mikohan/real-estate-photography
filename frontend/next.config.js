77cce035a6fa0ade851913931721f77e2ec2af54
manhee <angara99@gmail.com>
2023-08-14 15:08:38 -0700

    Changed headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: 'http://localhost' }, // replace this your actual origin
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Private-Network', value: true },
+          { key: 'Access-Control-Allow-Private-Network', value: 'true' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
