b21af95e8a81ad3dfb049cbdea7a3d87763a09eb
manhee <angara99@gmail.com>
2023-08-14 15:06:13 -0700

    Added policy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,11 +17,13 @@ const nextConfig = {
     return [
       {
         // matching all API routes
+        // Access-Control-Allow-Private-Network: true
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: 'http://localhost' }, // replace this your actual origin
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Private-Network', value: true },
           {
             key: 'Access-Control-Allow-Headers',
             value:
