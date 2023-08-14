b9e29565f0c72c84517bf68224ae621105cc40f4
manhee <angara99@gmail.com>
2023-08-14 16:18:16 -0700

    Added stuff

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -5,12 +5,22 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
-      headers: "*",
+      //    origin: ["*"],
       origin: ["http://localhost:1337", "http://angaramedia.site"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
     },
   },
   "strapi::poweredBy",
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     enabled: true,
+  //     headers: "*",
+  //     origin: ["http://localhost:1337", "http://angaramedia.site"],
+  //   },
+  // },
   "strapi::logger",
   "strapi::query",
   "strapi::body",
